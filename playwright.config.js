292f1329bae9d98b77ccd44d88fba16bc555370b
BHumi-repo <bhumikak280998@gmail.com>
2024-07-29 11:08:00 +0530

    chrome desktop option

--- playwright.config.js
+++ playwright.config.js
@@ -57,18 +57,18 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { 
-    //     //...devices['Desktop Chrome']
-    //     viewport:null
-    //    },
-    // },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'chromium',
+      use: { 
+        //...devices['Desktop Chrome']
+        viewport:null
+       },
     },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     // {
     //   name: 'webkit',
